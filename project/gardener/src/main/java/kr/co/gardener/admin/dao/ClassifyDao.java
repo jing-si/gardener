f501f47ac8c73dd40f73677d92dcf8bb6d84a2ca
Joung Ei Won <carrune@naver.com>
2021-08-29 17:56:17 +0900

    admin classify 완료

--- project/gardener/src/main/java/kr/co/gardener/admin/dao/ClassifyDao.java
+++ project/gardener/src/main/java/kr/co/gardener/admin/dao/ClassifyDao.java
@@ -6,10 +6,25 @@ import kr.co.gardener.admin.model.Classify;
 
 public interface ClassifyDao {
 
-	List<Classify> getTopClassList();
+	List<Classify> getList();
 
-	List<Classify> getMidClassList();
+	void topAdd(Classify item);
 
-	List<Classify> getBotClassList();
+	void midAdd(Classify item);
 
+	void botAdd(Classify item);
+
+	void topUpdate(Classify item);
+
+	void midUpdate(Classify item);
+
+	void botUpdate(Classify item);
+
+	void topDelete(int primaryId);
+
+	void midDelete(int primaryId);
+
+	void botDelete(int primaryId);
+
+	
 }
